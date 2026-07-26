7f538680fa1759fba879ab1eabe0d5572e4ce358
Pragyachaudhary07 <156224476+Pragyachaudhary07@users.noreply.github.com>
2026-07-26 06:42:19 +0000

    conversion operation done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+//alert("hello")
+console.log("Pragya")
+
+let name = "Pragya"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigInt
+// string => 
+// boolean => true/false
+// null => standalone value
+// undefined =>     
+// symbol => unique
+
+// object 
+
+console.log(typeof "Pragya");
+console.log(typeof null);
+console.log(typeof undefined);
