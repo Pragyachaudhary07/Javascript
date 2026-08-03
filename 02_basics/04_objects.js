75bec445eb373352ac12d751e4b79261df42ad3d
Pragyachaudhary07 <156224476+Pragyachaudhary07@users.noreply.github.com>
2026-08-03 14:58:14 +0000

    json talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -27,7 +27,7 @@ const obj4 = {5: "a", 6: "b"}
 //const obj3 = Object.assign({}, obj1, obj2, obj4)
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+//console.log(obj3);
 
 const users = [
     {
@@ -35,10 +35,35 @@ const users = [
         email: "h@gmail.com"
     },
 ]
-console.log(users[0].email);
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(users[0].email);
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor:"pragya"
+}
+
+// course.courseInstructor
+
+const{courseInstructor : instructor} = course
+
+console.log(instructor);
+
+
+// const navbar = ({company}) => {     Destructing
+// }
+// navbar(company = "pragya")
+
+
+// {
+//     name: "pragya"
+//     coursename: "js in hindi",
+//     price: "free"
+// }
+
