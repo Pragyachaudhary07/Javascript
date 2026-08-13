e7242d0f49e713f2b989cf65120fe2147c191d11
Pragyachaudhary07 <156224476+Pragyachaudhary07@users.noreply.github.com>
2026-08-13 21:02:38 +0000

    functions done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -33,4 +33,33 @@ function loginuserMessage(username = "sam"){
 }
 
 //console.log(loginuserMessage("pragya"))
-console.log(loginuserMessage())
+//console.log(loginuserMessage())
+
+
+function calculateCartPrice(val1, val2,...num1)
+{
+    return num1
+}
+
+//console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    username : "pragya",
+    price : 199
+}
+function handleObject(anyobject){
+      console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+handleObject({
+    username : "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[0]
+}
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
